fcf9c2fc79d35ddeb4ee37cde0a243e4476a2420
JemiJe <jeremiah7480@gmail.com>
2023-05-31 13:41:39 +0300

    index.js redacted

--- index.js
+++ index.js
@@ -5,22 +5,31 @@ const routes = require("./routes/routes");
 require("dotenv").config();
 const mongoString = process.env.DATABASE_URL;
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoString);
+    const database = mongoose.connection;
 
-database.on("error", (error) => {
-  console.log(error);
-});
+    database.on("error", (error) => {
+      console.log(error);
+    });
 
-database.once("connected", () => {
-  console.log("Database Connected");
-});
+    database.once("connected", () => {
+      console.log("Database Connected");
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
 
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`);
+connectDB().then(() => {
+  app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`);
+  });
 });
